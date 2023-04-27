9d2c17c0d91b29584896bb0d3078cf0ece5c7f04
Sklup55 <ksundararajan@skillup.tech>
2023-04-27 08:32:21 -0400

    commented codes for creating instructions

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import burger1 from "../../assets/burger1.png";
 import burger2 from "../../assets/burger2.png";
-import burger3 from "../../assets/burger3.png";
+// import burger3 from "../../assets/burger3.png";
 
 const CartItem = ({ value, title, img, increment, decrement }) => (
   <div className="cartItem">
@@ -32,22 +32,27 @@ const Cart = () => {
           img={burger1}
           value={0}
           increment={() => increment(1)}
-          decrement={() => decrement(1)}
+
+        // Add the function for decrementing the order by 1 
+        //   decrement={() => decrement(1)}
         />
         <CartItem
           title={"Veg Cheese Burger"}
           img={burger2}
           value={0}
           increment={() => increment(2)}
-          decrement={() => decrement(2)}
+        // Add the function for decrementing the order by 2
+        //   decrement={() => decrement(2)}
         />
-        <CartItem
+
+        {/* Fill up the code for Cheese Burger similarly */}
+        {/* <CartItem
           title={"Cheese Burger with French Fries"}
           img={burger3}
           value={0}
           increment={() => increment(3)}
           decrement={() => decrement(3)}
-        />
+        /> */}
 
         <article>
           <div>
