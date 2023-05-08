8fb6be40cd26f1b56aac2cdeb7964ed0aeddb56a
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2023-05-08 15:11:38 +0530

    Update Shipping.jsx

--- src/components/cart/Shipping.jsx
+++ src/components/cart/Shipping.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { State } from "country-state-city";
+import { Country, State } from "country-state-city";
 import Popup from 'reactjs-popup';
-import { Link } from "react-router-dom";
 
 const Shipping = () => {
   return (
@@ -18,44 +17,48 @@ const Shipping = () => {
             <input type="text" placeholder="Enter City" />
           </div>
           <div>
+              {/* Compelte the code for the COUNTRY DROPDOWN*/}
             <label>Country</label>
 
             <select>
-              <option value="">India</option>
-              {
-               }
+              <option value="">Country</option>
+//               {Country &&
+//                 Country.getAllCountries().map((i) => (
+//                   <option value={i.isoCode} key={i.isoCode}>
+//                     {i.name}
+//                   </option>
+                ))}
             </select>
           </div>
           <div>
-            <label>State</label>
+              {/* Add the code for the STATE DROPDOWN*/}
+//             <label>State</label>
 
-            <select>
-              <option value="">State</option>
-              {State &&
-                State.getStatesOfCountry("IN").map((i) => (
-                  <option value={i.isoCode} key={i.isoCode}>
-                    {i.name}
-                  </option>
-                ))}
-            </select>
+//             <select>
+//               <option value="">State</option>
+//               {State &&
+//                 State.getStatesOfCountry("IN").map((i) => (
+//                   <option value={i.isoCode} key={i.isoCode}>
+//                     {i.name}
+//                   </option>
+//                 ))}
+//             </select>
           </div>
           <div>
             <label>Pin Code</label>
             <input type="number" placeholder="Enter Pincode" />
           </div>
-          <div>
-            <label>Phone No.</label>
-            <input type="number" placeholder="Enter Phone No." />
-          </div>
+//           <div>
+//             <label>Phone No.</label>
+//             <input type="number" placeholder="Enter Phone No." />
+//           </div>
           
           <Popup trigger=
-                { <Link className="link" to="/myorders">Confirm Order</Link>}
-                
-               >
-                <div style={{color:"red", transform: 'translate(0%,-500%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>Order Successfully Placed!</div>
+                {<button type = "button">Confirm Order</button>}
+                position="right center">
+                <div style={{color:"red",position: 'absolute', top: '50%', right: '100%', transform: 'translateY(-50%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>Order Placed</div>
                
             </Popup>
-            
         </form>
       </main>
     </section>
