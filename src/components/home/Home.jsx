ccd3eaefbd190c6f311f9bb365829e70df97e6eb
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-26 17:49:57 +0200

    start task 1

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,11 @@
 // Write all the code here
+import React from 'react';
+import { motion } from 'framer-motion';
+import Founder from './Founder';
+import Menu from './Menu';
 
+const Home = () => {
+  return <div></div>;
+};
 
-
+export default Home;
