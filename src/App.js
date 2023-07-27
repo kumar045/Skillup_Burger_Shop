15de3a86387dd3b1dbe47ffcd86aca7a7947ba05
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-27 14:50:15 +0200

    feat: finish 1st task

--- src/App.js
+++ src/App.js
@@ -1,56 +1,69 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/home/Home";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
-import Contact from "./components/contact/Contact";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import Contact from './components/contact/Contact';
 // Add module imports for CART
 
 // Add module imports for Shipping
 
-import Login from "./components/login/Login";
-import Profile from "./components/profile/Profile";
+import Login from './components/login/Login';
+import Profile from './components/profile/Profile';
 // Add module imports for MY ORDERS
 
-import OrderDetails from "./components/myOrders/OrderDetails";
-import About from "./components/about/About";
+import OrderDetails from './components/myOrders/OrderDetails';
+import About from './components/about/About';
 
-
-import "./styles/app.scss";
-import "./styles/header.scss";
-import "./styles/home.scss";
-import "./styles/founder.scss";
-import "./styles/menu.scss";
-import "./styles/footer.scss";
-import "./styles/contact.scss";
-import "./styles/cart.scss";
-import "./styles/shipping.scss";
-import "./styles/login.scss";
-import "./styles/profile.scss";
-import "./styles/table.scss";
-import "./styles/orderDetails.scss";
-import "./styles/about.scss";
+import './styles/app.scss';
+import './styles/header.scss';
+import './styles/home.scss';
+import './styles/founder.scss';
+import './styles/menu.scss';
+import './styles/footer.scss';
+import './styles/contact.scss';
+import './styles/cart.scss';
+import './styles/shipping.scss';
+import './styles/login.scss';
+import './styles/profile.scss';
+import './styles/table.scss';
+import './styles/orderDetails.scss';
+import './styles/about.scss';
 
 function App() {
   return (
     <Router>
       <Header isAuthenticated={true} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-          //           Add the Route for CART
-      
-          //           Add the Route for SHIPPING
-        
-        <Route path="/login" element={<Login />} />
-        <Route path="/me" element={<Profile />} />
-         //           Add the Route for MY ORDERS
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/contact'
+          element={<Contact />}
+        />
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        {/* Add the Route for CART */}
 
-        <Route path="/order/:id" element={<OrderDetails />} />
-      
-       
+        {/* Add the Route for SHIPPING */}
 
-        
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/me'
+          element={<Profile />}
+        />
+        {/*  Add the Route for MY ORDERS */}
+
+        <Route
+          path='/order/:id'
+          element={<OrderDetails />}
+        />
       </Routes>
 
       <Footer />
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,25 @@
 // Write all the code here
 import React from 'react';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 import Founder from './Founder';
 import Menu from './Menu';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className='home'>
+        <div>
+          <h1>Burger Shop</h1>
+          <p>Give yourself a tasty burger.</p>
+        </div>
+
+        <a>Explore Menu</a>
+      </section>
+      <Menu />
+
+      <Founder />
+    </>
+  );
 };
 
 export default Home;
