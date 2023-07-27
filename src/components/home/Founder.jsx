894e6a82c579f4674a2bc7993479a288c0719144
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-27 15:00:04 +0200

    feat: finish 4th task

--- src/components/home/Founder.jsx
+++ src/components/home/Founder.jsx
@@ -1,30 +1,24 @@
-import React from "react";
-import { motion } from "framer-motion";
-import me from "../../assets/skj.jpg";
+import React from 'react';
+// import { motion } from "framer-motion";
+import me from '../../assets/skj.jpg';
 const Founder = () => {
-  const options = {
-    initial: {
-      x: "-100%",
-      opacity: 0,
-    },
-    whileInView: {
-      x: 0,
-      opacity: 1,
-    },
-  };
-
   return (
-    <section className="founder">
-      <motion.div {...options}>
-        <img src={me} alt="Founder" height={200} width={200} />
-        <h3>Nelson</h3>
+    <section className='founder'>
+      <div>
+        <img
+          src={me}
+          alt='Founder'
+          height={200}
+          width={200}
+        />
+        <h3>Elena</h3>
 
         <p>
-          Hey, Everyone I am Nelson, the founder of Burger Shop.
+          Hey, Everyone I am the founder of Burger Shop.
           <br />
           Our aim is to create the most tasty burger on planet.
         </p>
-      </motion.div>
+      </div>
     </section>
   );
 };
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
       </section>
       <Menu />
 
-      <Founder />
+      {/* <Founder /> */}
     </>
   );
 };
