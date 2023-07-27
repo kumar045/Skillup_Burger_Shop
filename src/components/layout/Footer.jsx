1b103902fc7ce39f2344b0d5152c7fb937fed1a4
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-27 14:56:38 +0200

    feat: finish 3rd task

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,1 +1,32 @@
-// Write all the code here
+import React from 'react';
+import { AiFillInstagram, AiFillYoutube } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2>Burger Shop</h2>
+
+        <p>We are trying to give you the best taste possible.</p>
+        <br />
+
+        <em>We give attention to genuine feedback.</em>
+
+        <strong>All right received @burgershop</strong>
+      </div>
+
+      <aside>
+        <h4>Follow Us</h4>
+
+        <a href='./'>
+          <AiFillYoutube />
+        </a>
+        <a href='./'>
+          <AiFillInstagram />
+        </a>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
