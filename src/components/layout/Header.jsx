7ad0c6271e160fb2628f3631d7fe9953531f4d1c
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-27 14:52:35 +0200

    feat: finish 2nd task

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,1 +1,28 @@
-// Write all the code here
+import React from 'react';
+import { IoFastFoodOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import { FiShoppingCart, FiLogIn } from 'react-icons/fi';
+// import { FaUser } from 'react-icons/fa';
+// import { motion } from 'framer-motion';
+import DropdownMenu from './DropdownMenu';
+
+const Header = ({ isAuthenticated = false }) => {
+  return (
+    <nav>
+      <div>
+        <IoFastFoodOutline />
+      </div>
+
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contact</Link> <Link to='/about'>About</Link>
+        <Link to='/cart'>
+          <FiShoppingCart />
+        </Link>
+        <DropdownMenu />
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
