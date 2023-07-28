1912217d4504d223c9ddaec3328d67ad877d28d6
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-28 14:59:30 +0200

    feat: complete task 2-2

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,1 +1,45 @@
-// Write all the code here
+import React from 'react';
+import { motion } from 'framer-motion';
+import burger from '../../assets/burger2.png';
+import Popup from 'reactjs-popup';
+
+const Contact = () => {
+  return (
+    <section className='contact'>
+      <form>
+        <h2>Contact Us</h2>
+        <input
+          type='text'
+          placeholder='Name'
+        />
+        <input
+          type='email'
+          placeholder='Email'
+        />
+
+        <textarea
+          placeholder='Message...'
+          cols='30'
+          rows='10'
+        ></textarea>
+
+        <Popup trigger={<button type='button'>Send</button>}>
+          <div
+            style={{
+              color: 'red',
+              transform: 'translate(25%,-700%)',
+              backgroundColor: '#fff',
+              padding: '10px',
+              borderRadius: '5px',
+              boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            }}
+          >
+            Thank you for cotacting us! We will get back to you shortly.
+          </div>
+        </Popup>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
