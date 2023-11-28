e70c2a5d6d64ba8ed3998418da4043df8e03b5e5
Sangeetha Kannan <sangeethasubburajj@gmail.com>
2023-11-29 02:24:57 +0530

    Contact page complete

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,1 +1,44 @@
-// Write all the code here
+import React, { useState } from 'react';
+import "../../styles/contact.scss";
+import burgersImage from "../../assets/burger2.png"
+
+const Contact = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowPopup(true);
+    // You can add additional logic or actions here
+  };
+
+  return (
+    <div className="contact">
+      <form>
+        <h2>Contact Us</h2>
+        {/* Your form input fields go here */}
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <textarea placeholder="Message" rows="8" cols="50"></textarea>
+        <button type="button" onClick={handleButtonClick}>
+          Send
+        </button>
+        {showPopup && (
+        <div className="popup">
+          {/* <p>Thank you for contacting us!We will get back to you shortly.</p> */}
+          <div style={{color:"red", transform: 'translate(0%,-500%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>Thank you for contacting us!We will get back to you shortly.</div>
+          <button onClick={() => setShowPopup(false)}></button>
+        </div>
+      )}
+      </form>
+
+      <div className="formBorder">
+        <div>
+          {/* You might want to replace the img tag with your actual image */}
+          <img src={burgersImage} alt="Form Border Image" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
+
