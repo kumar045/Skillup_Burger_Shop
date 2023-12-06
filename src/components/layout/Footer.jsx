4b37fecd06f46a4d162ef7d896c9ed40b00e99c1
Maye Jesuorobo <nayawes@yahoo.com>
2023-12-06 23:51:53 +0100

    Setup layout for footer section

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,7 @@
 // Write all the code here
+import youTube from "../../assets/svg/youtube-svgrepo-com.svg"
+import instagram from "../../assets/svg/instagram-svgrepo-com.svg"
+import github from "../../assets/svg/github-svgrepo-com.svg"
 
 function Footer() {
   return (
@@ -6,16 +9,22 @@ function Footer() {
         <div>
             <footer>
                 <div>
-                    <h6>Burger Shop</h6>
+                    <h2>Burger Shop</h2>
                     <p>We are trying to give you the best taste possible</p>
                     <p>We give attention to genuine feedback. <span>All tights reserved @burgershop</span></p>
                 </div>
-                <div>
-                    <small>Follow Us</small>
-                    <div className="icons">Icon 1</div>
-                    <div className="icons">Icon 2</div>
-                    <div className="icons">Icon 3</div>
-                </div>
+                <aside className="footer-icons">
+                    <h4>Follow Us</h4>
+                    <a href="#" className="icons tube">
+                        <img src={youTube} alt="youTube icon" width={"35px"}/>
+                    </a>
+                    <a href="#" className="icons tube">
+                        <img src={instagram} alt="instagram icon" width={"35px"} />
+                    </a>
+                    <a href="#" className="icons tube">
+                        <img src={github} alt="github icon" width={"40px"} />
+                    </a>
+                </aside>
             </footer>
         </div>
     </>
