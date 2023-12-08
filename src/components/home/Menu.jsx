fc9f8cf81b62a32f35cc324d13bae8b94b685838
Maye Jesuorobo <nayawes@yahoo.com>
2023-12-08 15:26:43 +0100

    add mapped MenuCard component

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -1,1 +1,27 @@
 // Write all the code here
+import "../../styles/menu.scss"
+import MenuCard from "./MenuCard"
+import menuData from "../../menuData"
+
+function Menu() {
+  return (
+    <>
+        <div id="menu">
+            <h3>Menu</h3>
+            <div>
+                {menuData.map(item => (
+                    <MenuCard 
+                    key={item.itemNum}
+                    itemNum={item.itemNum}
+                    burgerSrc={item.burgerSrc}
+                    price={item.price}
+                    title={item.title}
+                    />
+                )) }
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Menu
