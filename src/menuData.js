51a248fc8257d9e40e625e41e154d787e34bccb7
Maye Jesuorobo <nayawes@yahoo.com>
2023-12-08 15:27:28 +0100

    add data to be mapped through Menu component

---
+++ src/menuData.js
@@ -0,0 +1,11 @@
+import burger1 from "./assets/burger1.png";
+import burger2 from "./assets/burger2.png";
+import burger3 from "./assets/burger3.png";
+
+const menuData = [
+    { itemNum: 1, burgerSrc: burger1, price: 200, title: 'Cheese burger' },
+    { itemNum: 2, burgerSrc: burger2, price: 500, title: 'Veg cheese burger' },
+    { itemNum: 3, burgerSrc: burger3, price: 1800, title: 'Cheese burger with french fries' },
+]
+
+export default menuData;
