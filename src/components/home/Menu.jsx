857c564ee266dc7263d6fe4220022c2a380609c8
Kelroy James <76266693+kelroyjames@users.noreply.github.com>
2023-12-15 15:39:18 +0000

    Main

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -1,1 +1,46 @@
-// Write all the code here
+import React from "react";
+import MenuCard from "./MenuCard";
+import burger1 from "../../assets/burger1.png";
+import burger2 from "../../assets/burger2.png";
+import burger3 from "../../assets/burger3.png";
+<MenuCard
+ itemNum={1}
+ burgerSrc={burger1}
+ price={200}
+ title="Cheese Burger"
+ handler={addToCartHandler}
+ delay={0.1}
+/>
+import React, { useState, useEffect } from 'react';
+import './MenuCard.css';
+
+const MenuCard = ({ item, handleAddToCart }) => {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 500);
+  }, [item.title]);
+
+  const handleClick = () => {
+    handleAddToCart(item);
+  };
+
+  return (
+    <div className={`menu-card ${isAnimating ? 'animate-in' : ''}`}>
+      <div className="image-container">
+        <img src={item.image} alt={item.title} />
+      </div>
+      <div className="content">
+        <h3>{item.title}</h3>
+        <p>₹{item.price}</p>
+        <button onClick={handleClick}>Add to Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default MenuCard;
+
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,1 +1,32 @@
-// Write all the code here
+import React from "react";
+import { IoFastFoodOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { FiShoppingCart, FiLogIn } from "react-icons/fi";
+import { FaUser } from "react-icons/fa";
+import { motion } from "framer-motion";
+import DropdownMenu from "./DropdownMenu";
+const Header = ({ isAuthenticated = false }) => {
+ return (
+ <nav>
+ <motion.div initial={{ x: "-100%" }} whileInView={{ x: 0 }}>
+ <IoFastFoodOutline />
+ </motion.div>
+ <div>
+ <Link to="/">Home</Link>
+ <Link to="/contact">Contact</Link>
+ <Link to="/about">About</Link>
+ <Link to="/cart">
+ <FiShoppingCart />
+ </Link>
+ {isAuthenticated ? (
+ <DropdownMenu />
+ ) : (
+ <Link to="/login">
+ <FiLogIn />
+ </Link>
+ )}
+ </div>
+ </nav>
+ );
+};
+export default Header;
